ee23b6f0bcbe78bd047daf9ada4a9187517e5488
qinshiqiang <qinshiqiang@addnewer.com>
2018-10-22 17:19:30 +0800

    增加自定义函数

--- common/Func.py
+++ common/Func.py
@@ -43,6 +43,32 @@ def deleteDataSetByName(token, datasetname):
 	resp = TestRequest().test_request(url=url, method='post', headers=headers, data=request_data)
 	return False if resp[0] != 200 or resp[1]['code'] != 0 else True
 
+def deleteCampaignByName(token, campaignname):
+	if campaignname ==  'not_defined':
+		return True
+	Data = TestData.GetData()
+	host = Data.get_config_data('Host', 'cm_host')
+	api = '/apicm/Page/Campaign/Campaign/getRmosList'
+	url = host + api
+	request_data = json.dumps(Data.get_yml_data(Data.change_api_name(api))['json'])
+	headers = {'token': token}
+	resp = TestRequest().test_request(url=url, method='post', headers=headers, data=request_data)
+	if resp[0] != 200:return False
+	data_list = resp[1]['data']['list']
+	data_id = ''
+	for data in data_list:
+		if data['name'] == campaignname:
+			data_id = data['id']
+	if data_id == '':
+		return True
+	delete_api = '/apicm/Page/Campaign/Campaign/deleteRmosCampaign'
+	url = host + delete_api
+	request_data = Data.get_yml_data(Data.change_api_name(delete_api))['json']
+	request_data['rmos_campaign_id'] = int(data_id)
+	request_data = json.dumps(request_data)
+	headers = {'token': token}
+	resp = TestRequest().test_request(url=url, method='post', headers=headers, data=request_data)
+	return False if resp[0] != 200 or resp[1]['code'] != 0 else True
 
 if __name__ == '__main__':
-	deleteDataSetByName('db71926e552750f8a470d338950dd17a3fc4ffec','test')
+	deleteCampaignByName('77ce39ab2ca123899e1a831e1d1a9f01bd83fe44','aa')
